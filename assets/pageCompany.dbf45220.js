63e96ed94dee845857c3ee36012fad6d2f7106bc
Val Saven <val.saven@gmail.com>
2023-03-06 12:23:41 +0300

    v0.1

---
+++ assets/pageCompany.dbf45220.js
@@ -0,0 +1,1 @@
+import{d as e,_ as o,o as t,c as n}from"./index.7225928e.js";const s=e({data(){return{}},computed:{},methods:{},mounted(){}}),a={class:"company"};function c(r,p,d,_,m,f){return t(),n("div",a)}const u=o(s,[["render",c]]);export{u as default};
